97b55ca1521d495ea420b59aed1d3c6404b43aa1
sivatejadesi <114158599+sivatejadesi@users.noreply.github.com>
2022-09-23 18:05:58 +0530

    Added files via upload

---
+++ catalogservice/src/main/java/com/teja/catalogservice/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.teja.catalogservice.repositories;
+
+import com.teja.catalogservice.entities.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+ 
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByCode(String code);
+}
---
+++ catalogservice/src/main/java/com/teja/catalogservice/web/controller/ProductController.java
@@ -0,0 +1,37 @@
+package com.teja.catalogservice.web.controller;
+
+import com.teja.catalogservice.entities.Product;
+import com.teja.catalogservice.exceptions.ProductNotFoundException;
+import com.teja.catalogservice.services.ProductService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
+import java.util.List;
+ 
+@RestController
+@RequestMapping("/api/products")
+@Slf4j
+public class ProductController {
+ 
+    private final ProductService productService;
+ 
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+ 
+    @GetMapping("")
+    public List<Product> allProducts() {
+        return productService.findAllProducts();
+    }
+ 
+    @GetMapping("/{code}")
+    public Product productByCode(@PathVariable String code) {
+        return productService.findProductByCode(code)
+                .orElseThrow(() -> new ProductNotFoundException("Product with code ["+code+"] doesn't exist"));
+    }
+}
